5c76375f74f8d336f95a3e2a15079d92f4a5a283
azhuge233 <17763056+azhuge233@users.noreply.github.com>
2021-05-10 17:27:50 +0800

    Create BattleNetCacheClean.ps1
    Add Battle.net Cache Cleaner

---
+++ BattleNetCacheClean.ps1
@@ -0,0 +1,9 @@
+﻿$battleNetBrowserCachePath = "$env:USERPROFILE\AppData\Local\Battle.net\BrowserCache\Cache"
+$battleNetCachePath = "$env:USERPROFILE\AppData\Local\Battle.net\Cache"
+$battleNetProgramDataCachePath = "$env:PROGRAMDATA\Blizzard Entertainment\Battle.net\Cache"
+$battleNetProgramDataLogPath = "$env:PROGRAMDATA\Battle.net\Agent\Agent.*\Logs"
+
+Get-ChildItem -Path $battleNetCachePath -Recurse -Force | Remove-Item -Verbose -Recurse
+Get-ChildItem -Path $battleNetProgramDataCachePath -Recurse -Force | Remove-Item -Verbose -Recurse
+Get-ChildItem -Path $battleNetBrowserCachePath -Recurse -Force -Filter f_* | Remove-Item -Verbose
+Get-ChildItem -Path $battleNetProgramDataLogPath -Recurse -Force -Filter *.log | Remove-Item -Verbose
