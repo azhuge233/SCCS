16eddce60d15a6778752e2be63324a68ae98f82f
azhuge233 <17763056+azhuge233@users.noreply.github.com>
2021-05-12 20:37:03 +0800

    Create HeroesOfTheStormReplayClean.ps1
    Add Heroes of the Storm replay files cleaner.

---
+++ HeroesOfTheStormReplayClean.ps1
@@ -0,0 +1,3 @@
+﻿$heroesOfTheStormReplayPath = "$env:USERPROFILE\Documents\Heroes of the Storm\Accounts\*\*-Hero-*\Replays\Multiplayer\*"
+
+Get-ChildItem -Path $heroesOfTheStormReplayPath -Filter *.StormReplay | Remove-Item -Verbose
