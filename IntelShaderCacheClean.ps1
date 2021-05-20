64016106d5f79c45cdceab980b2b42a0e1b60957
azhuge233 <17763056+azhuge233@users.noreply.github.com>
2021-05-20 17:43:11 +0800

    Create IntelShaderCacheClean.ps1
    Add Intel shader cache cleaning script.

---
+++ IntelShaderCacheClean.ps1
@@ -0,0 +1,7 @@
+﻿$intelGrapgicsShaderCachePath = "$env:USERPROFILE\Appdata\LocalLow\Intel\ShaderCache"
+
+try {
+    Get-ChildItem -Path $intelGrapgicsShaderCachePath -Force -ErrorAction Stop | Remove-Item -Verbose
+} catch [System.Management.Automation.ItemNotFoundException] {
+    "Can't find $($intelGrapgicsShaderCachePath).`nApplication may not be installed on this machine, or the folders aren't created yet."
+}
