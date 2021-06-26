7a2e5ed90c7d1d2aa5629a74c825563371504148
azhuge233 <17763056+azhuge233@users.noreply.github.com>
2021-06-26 08:43:09 +0800

    Create AMDDxCacheClean.ps1
    Add AMD DxCache cleaner

---
+++ AMDDxCacheClean.ps1
@@ -0,0 +1,7 @@
+﻿$AMDDxCachePath = "$env:USERPROFILE\AppData\Local\AMD\DxCache"
+
+try {
+    Get-ChildItem -Path $AMDDxCachePath -Recurse -Force -ErrorAction Stop | Remove-Item -Verbose -Recurse
+} catch [System.Management.Automation.ItemNotFoundException] {
+    "Can't find $($AMDDxCachePath).`nApplication may not be installed on this machine, or the folders aren't created yet."
+}
