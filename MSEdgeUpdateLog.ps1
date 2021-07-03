49e162a177b0d504bbd10d49ab66923837ea8927
azhuge233 <17763056+azhuge233@users.noreply.github.com>
2021-07-04 06:12:30 +0800

    Create MSEdgeUpdateLog.ps1
    Add Microsoft Edge update log cleaner

---
+++ MSEdgeUpdateLog.ps1
@@ -0,0 +1,7 @@
+﻿$msEdgeUpdateLogPath = "$env:PROGRAMDATA\Microsoft\EdgeUpdate\Log"
+
+try {
+    Get-ChildItem -Path $msEdgeUpdateLogPath -Recurse -Force -ErrorAction Stop | Remove-Item -Verbose
+} catch [System.Management.Automation.ItemNotFoundException] {
+    "Can't find $($msEdgeUpdateLogPath).`nApplication may not be installed on this machine, or the folders aren't created yet."
+}
