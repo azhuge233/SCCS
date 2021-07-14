c4471549d73be9557bb05d1a3e50b20a965f112a
azhuge233 <17763056+azhuge233@users.noreply.github.com>
2021-07-14 12:14:48 +0800

    Create OriginCacheClean.ps1
    Add Origin cache cleaner

---
+++ OriginCacheClean.ps1
@@ -0,0 +1,7 @@
+﻿$originCachePath = "$env:USERPROFILE\AppData\Local\Origin\Origin\cache\QtWebEngine\Default\Cache"
+
+try {
+    Get-ChildItem -Path $originCachePath -Filter f_* -ErrorAction Stop | Remove-Item -Verbose
+} catch [System.Management.Automation.ItemNotFoundException] {
+    "Can't find $($originCachePath).`nApplication may not be installed on this machine, or the folders aren't created yet."
+}
