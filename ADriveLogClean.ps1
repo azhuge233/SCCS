90fb19ef9a4e1c0191d88f336c6b896dcce8bbf6
azhuge233 <17763056+azhuge233@users.noreply.github.com>
2021-07-24 23:48:20 +0800

    Create ADriveLogClean.ps1

---
+++ ADriveLogClean.ps1
@@ -0,0 +1,14 @@
+﻿$aDriveLogPath1 = "$env:USERPROFILE\AppData\Roaming\aDrive\aDrive\logs"
+$aDriveLogPath2 = "$env:USERPROFILE\AppData\Roaming\aDrive\logs"
+
+try {
+    Get-ChildItem -Path $aDriveLogPath1 -Recurse -Force -Filter *.log -ErrorAction Stop | Remove-Item -Verbose
+} catch [System.Management.Automation.ItemNotFoundException] {
+    "Can't find $($aDriveLogPath1).`nApplication may not be installed on this machine, or the folders aren't created yet."
+}
+
+try {
+    Get-ChildItem -Path $aDriveLogPath2 -Recurse -Force -Filter *.log -ErrorAction Stop | Remove-Item -Verbose
+} catch [System.Management.Automation.ItemNotFoundException] {
+    "Can't find $($aDriveLogPath2).`nApplication may not be installed on this machine, or the folders aren't created yet."
+}
