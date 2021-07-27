f5cec19ad09c34ad31ce930b5eea434833f5a6cc
azhuge233 <17763056+azhuge233@users.noreply.github.com>
2021-07-27 13:12:29 +0800

    Update ADriveLogClean.ps1
    Add aDrive update package path

--- ADriveLogClean.ps1
+++ ADriveLogClean.ps1
@@ -1,5 +1,6 @@
 ﻿$aDriveLogPath1 = "$env:USERPROFILE\AppData\Roaming\aDrive\aDrive\logs"
 $aDriveLogPath2 = "$env:USERPROFILE\AppData\Roaming\aDrive\logs"
+$aDriveUpdatePackagePath = "$env:USERPROFILE\AppData\Local\adrive-desktop-updater\pending"
 
 try {
     Get-ChildItem -Path $aDriveLogPath1 -Recurse -Force -Filter *.log -ErrorAction Stop | Remove-Item -Verbose
@@ -11,4 +12,10 @@ try {
     Get-ChildItem -Path $aDriveLogPath2 -Recurse -Force -Filter *.log -ErrorAction Stop | Remove-Item -Verbose
 } catch [System.Management.Automation.ItemNotFoundException] {
     "Can't find $($aDriveLogPath2).`nApplication may not be installed on this machine, or the folders aren't created yet."
+}
+
+try {
+    Get-ChildItem -Path $aDriveUpdatePackagePath -Recurse -Force -ErrorAction Stop | Remove-Item -Verbose
+} catch [System.Management.Automation.ItemNotFoundException] {
+    "Can't find $($aDriveUpdatePackagePath).`nApplication may not be installed on this machine, or the folders aren't created yet."
 }
