9d716365daaa85b4a7304bbc014aea1dad941dca
azhuge233 <17763056+azhuge233@users.noreply.github.com>
2021-07-28 08:59:25 +0800

    Update BaiduNetDiskUpdateClean.ps1
    BaiduNetDiskUpdateClean: add flashplayer.dll, browser module deletion.

--- BaiduNetDiskUpdateClean.ps1
+++ BaiduNetDiskUpdateClean.ps1
@@ -1,7 +1,21 @@
-﻿$baiduNetDiskUpdatePath = "$env:USERPROFILE\AppData\Roaming\baidu\BaiduNetdisk\AutoUpdate\Download\MainApp"
+﻿$baiduNetDiskUpdatePackagePath = "$env:USERPROFILE\AppData\Roaming\baidu\BaiduNetdisk\AutoUpdate\Download\MainApp"
+$baiduNetDiskPepFlashPlayerPath = "$env:USERPROFILE\AppData\Roaming\baidu\BaiduNetdisk\pepflashplayer.dll"
+$baiduNetDiskBrowserModuleUpdatePath = "$env:USERPROFILE\AppData\Roaming\baidu\BaiduNetdisk\module\browserengine"
 
 try {
-    Get-ChildItem -Path $baiduNetDiskUpdatePath -ErrorAction Stop | Remove-Item -Verbose
+    Get-ChildItem -Path $baiduNetDiskUpdatePackagePath -ErrorAction Stop | Remove-Item -Verbose
 } catch [System.Management.Automation.ItemNotFoundException] {
-    "Can't find $($baiduNetDiskUpdatePath).`nApplication may not be installed on this machine, or the folders aren't created yet."
+    "Can't find $($baiduNetDiskUpdatePackagePath).`nApplication may not be installed on this machine, or the folders aren't created yet."
+}
+
+try {
+    Get-ChildItem -Path $baiduNetDiskPepFlashPlayerPath -ErrorAction Stop | Remove-Item -Force
+} catch [System.Management.Automation.ItemNotFoundException] {
+    "Can't find $($baiduNetDiskPepFlashPlayerPath).`nApplication may not be installed on this machine, or the folders aren't created yet."
+}
+
+try {
+    Get-ChildItem -Path $baiduNetDiskBrowserModuleUpdatePath -ErrorAction Stop | Remove-Item -Verbose -Force -Recurse
+} catch [System.Management.Automation.ItemNotFoundException] {
+    "Can't find $($baiduNetDiskBrowserModuleUpdatePath).`nApplication may not be installed on this machine, or the folders aren't created yet."
 }
