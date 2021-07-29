f8988302f75f57dfadaea40edff4d64f926d4adc
azhuge233 <17763056+azhuge233@users.noreply.github.com>
2021-07-29 19:47:25 +0800

    Update BaiduNetDiskUpdateClean.ps1
    Add Baidu Net Disk kernel log path.

--- BaiduNetDiskUpdateClean.ps1
+++ BaiduNetDiskUpdateClean.ps1
@@ -1,6 +1,7 @@
 ﻿$baiduNetDiskUpdatePackagePath = "$env:USERPROFILE\AppData\Roaming\baidu\BaiduNetdisk\AutoUpdate\Download\MainApp"
 $baiduNetDiskPepFlashPlayerPath = "$env:USERPROFILE\AppData\Roaming\baidu\BaiduNetdisk\pepflashplayer.dll"
 $baiduNetDiskBrowserModuleUpdatePath = "$env:USERPROFILE\AppData\Roaming\baidu\BaiduNetdisk\module\browserengine"
+$baiduNetDiskKernelLogPath = "$env:USERPROFILE\AppData\Roaming\BaiduYunKernel\Data"
 
 try {
     Get-ChildItem -Path $baiduNetDiskUpdatePackagePath -ErrorAction Stop | Remove-Item -Verbose
@@ -18,4 +19,10 @@ try {
     Get-ChildItem -Path $baiduNetDiskBrowserModuleUpdatePath -ErrorAction Stop | Remove-Item -Verbose -Force -Recurse
 } catch [System.Management.Automation.ItemNotFoundException] {
     "Can't find $($baiduNetDiskBrowserModuleUpdatePath).`nApplication may not be installed on this machine, or the folders aren't created yet."
+}
+
+try {
+    Get-ChildItem -Path $baiduNetDiskKernelLogPath -Filter *.log -ErrorAction Stop | Remove-Item -Verbose -Force -Recurse
+} catch [System.Management.Automation.ItemNotFoundException] {
+    "Can't find $($baiduNetDiskKernelLogPath).`nApplication may not be installed on this machine, or the folders aren't created yet."
 }
