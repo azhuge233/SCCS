cf7ec7a83903c1ce578a35e30f78f124c491347c
azhuge233 <17763056+azhuge233@users.noreply.github.com>
2021-08-04 21:56:40 +0800

    Update PowerToysLogClean.ps1
    Add powertoys run log path.

--- PowerToysLogClean.ps1
+++ PowerToysLogClean.ps1
@@ -1,7 +1,14 @@
 ﻿$PowerToysLogPath = "$env:USERPROFILE\AppData\Local\Microsoft\PowerToys"
+$PowerToysRunLogPath = "$env:USERPROFILE\AppData\Local\Microsoft\PowerToys\PowerToys Run\Logs"
 
 try {
     Get-ChildItem -Path $PowerToysLogPath -Recurse -Filter *.log -Force -ErrorAction Stop | Remove-Item -Verbose -Recurse
 } catch [System.Management.Automation.ItemNotFoundException] {
     "Can't find $($PowerToysLogPath).`nApplication may not be installed on this machine, or the folders aren't created yet."
+}
+
+try {
+    Get-ChildItem -Path $PowerToysRunLogPath -Recurse -Force -ErrorAction Stop | Remove-Item -Verbose -Recurse
+} catch [System.Management.Automation.ItemNotFoundException] {
+    "Can't find $($PowerToysRunLogPath).`nApplication may not be installed on this machine, or the folders aren't created yet."
 }
