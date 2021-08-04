20508bf0b80e1d8ebc246b88ef8f8649c771dd89
azhuge233 <17763056+azhuge233@users.noreply.github.com>
2021-08-04 21:32:31 +0800

    Create PowerToysLogClean.ps1

---
+++ PowerToysLogClean.ps1
@@ -0,0 +1,7 @@
+﻿$PowerToysLogPath = "$env:USERPROFILE\AppData\Local\Microsoft\PowerToys"
+
+try {
+    Get-ChildItem -Path $PowerToysLogPath -Recurse -Filter *.log -Force -ErrorAction Stop | Remove-Item -Verbose -Recurse
+} catch [System.Management.Automation.ItemNotFoundException] {
+    "Can't find $($PowerToysLogPath).`nApplication may not be installed on this machine, or the folders aren't created yet."
+}
