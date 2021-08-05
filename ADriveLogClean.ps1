244a87976193aae664c6ab30cab542c54bce70ea
azhuge233 <17763056+azhuge233@users.noreply.github.com>
2021-08-05 17:28:48 +0800

    Update ADriveLogClean.ps1
    Add Adrive partition cache path.

--- ADriveLogClean.ps1
+++ ADriveLogClean.ps1
@@ -1,6 +1,7 @@
 ﻿$aDriveLogPath1 = "$env:USERPROFILE\AppData\Roaming\aDrive\aDrive\logs"
 $aDriveLogPath2 = "$env:USERPROFILE\AppData\Roaming\aDrive\logs"
 $aDriveUpdatePackagePath = "$env:USERPROFILE\AppData\Local\adrive-desktop-updater\pending"
+$aDriveLPartitionCachePath = "$env:USERPROFILE\AppData\Roaming\aDrive\Partitions\adrive\Cache"
 
 try {
     Get-ChildItem -Path $aDriveLogPath1 -Recurse -Force -Filter *.log -ErrorAction Stop | Remove-Item -Verbose
@@ -18,4 +19,10 @@ try {
     Get-ChildItem -Path $aDriveUpdatePackagePath -Recurse -Force -ErrorAction Stop | Remove-Item -Verbose
 } catch [System.Management.Automation.ItemNotFoundException] {
     "Can't find $($aDriveUpdatePackagePath).`nApplication may not be installed on this machine, or the folders aren't created yet."
+}
+
+try {
+    Get-ChildItem -Path $aDriveLPartitionCachePath -Recurse -Filter f_* -Force -ErrorAction Stop | Remove-Item -Verbose
+} catch [System.Management.Automation.ItemNotFoundException] {
+    "Can't find $($aDriveLPartitionCachePath).`nApplication may not be installed on this machine, or the folders aren't created yet."
 }
