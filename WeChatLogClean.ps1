b14f617da1f4c57ee09461892cd5a03f30f3246d
azhuge233 <17763056+azhuge233@users.noreply.github.com>
2021-08-23 17:28:22 +0800

    Create WeChatLogClean.ps1

---
+++ WeChatLogClean.ps1
@@ -0,0 +1,7 @@
+﻿$weChatLogPath = "$env:USERPROFILE\AppData\Roaming\Tencent\WeChat\log"
+
+try {
+    Get-ChildItem -Path $weChatLogPath -Recurse -Force -Filter *.*log -ErrorAction Stop | Remove-Item -Verbose
+} catch [System.Management.Automation.ItemNotFoundException] {
+    "Can't find $($aDriveLogPath1).`nApplication may not be installed on this machine, or the folders aren't created yet."
+}
