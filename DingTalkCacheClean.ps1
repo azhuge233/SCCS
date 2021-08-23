ab250bfa6f25b2bcf0d594c2ce787569fb1a4845
azhuge233 <17763056+azhuge233@users.noreply.github.com>
2021-08-23 17:28:00 +0800

    Create DingTalkCacheClean.ps1

---
+++ DingTalkCacheClean.ps1
@@ -0,0 +1,22 @@
+﻿$dingTalkCachePath = "$env:USERPROFILE\AppData\Local\DingTalk_*\Cache"
+$dingTalkCodeCacheJSPath = "$env:USERPROFILE\AppData\Local\DingTalk_*\Code Cache\js"
+$dingTalkCodeCacheWASMPath = "$env:USERPROFILE\AppData\Local\DingTalk_*\Code Cache\wasm"
+
+try {
+    Get-ChildItem -Path $dingTalkCachePath -Recurse -Force -Filter f_* -ErrorAction Stop | Remove-Item -Verbose
+} catch [System.Management.Automation.ItemNotFoundException] {
+    "Can't find $($aDriveLogPath1).`nApplication may not be installed on this machine, or the folders aren't created yet."
+}
+
+try {
+    Get-ChildItem -Path $dingTalkCodeCacheJSPath -Recurse -Force -Filter *_0 -ErrorAction Stop | Remove-Item -Verbose
+} catch [System.Management.Automation.ItemNotFoundException] {
+    "Can't find $($aDriveLogPath1).`nApplication may not be installed on this machine, or the folders aren't created yet."
+}
+
+try {
+    Get-ChildItem -Path $dingTalkCodeCacheWASMPath -Recurse -Force -Filter *_0 -ErrorAction Stop | Remove-Item -Verbose
+} catch [System.Management.Automation.ItemNotFoundException] {
+    "Can't find $($aDriveLogPath1).`nApplication may not be installed on this machine, or the folders aren't created yet."
+}
+
