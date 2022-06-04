a569726398d782a6790e736e5c7a53bfca3df277
azhuge233 <17763056+azhuge233@users.noreply.github.com>
2022-06-04 08:04:02 +0800

    Update PowerToysLogClean.ps1
    Add FancyZone log clean.

--- PowerToysLogClean.ps1
+++ PowerToysLogClean.ps1
@@ -1,5 +1,6 @@
 ﻿$PowerToysLogPath = "$env:USERPROFILE\AppData\Local\Microsoft\PowerToys"
 $PowerToysRunLogPath = "$env:USERPROFILE\AppData\Local\Microsoft\PowerToys\PowerToys Run\Logs"
+$FancyZoneLogPath = "$env:USERPROFILE\AppData\Local\Microsoft\PowerToys\FancyZones\Logs"
 
 try {
     Get-ChildItem -Path $PowerToysLogPath -Recurse -Filter *.log -Force -ErrorAction Stop | Remove-Item -Verbose -Recurse
@@ -11,4 +12,10 @@ try {
     Get-ChildItem -Path $PowerToysRunLogPath -Recurse -Force -ErrorAction Stop | Remove-Item -Verbose -Recurse
 } catch [System.Management.Automation.ItemNotFoundException] {
     "Can't find $($PowerToysRunLogPath).`nApplication may not be installed on this machine, or the folders aren't created yet."
+}
+
+try {
+    Get-ChildItem -Path $FancyZoneLogPath -Recurse -Force -ErrorAction Stop | Remove-Item -Verbose -Recurse
+} catch [System.Management.Automation.ItemNotFoundException] {
+    "Can't find $($FancyZoneLogPath).`nApplication may not be installed on this machine, or the folders aren't created yet."
 }
