15a0b017157baca7c81275f84b1d57dce24c981d
azhuge233 <17763056+azhuge233@users.noreply.github.com>
2022-08-09 19:01:46 +0800

    Update SteamClean.ps1
    Fix SteamVR issue.

--- SteamClean.ps1
+++ SteamClean.ps1
@@ -9,15 +9,15 @@ $SteamVRCachePath = "$env:USERPROFILE\AppData\Local\SteamVR\htmlcache\Cache"
 $SteamVRGPUCachePath = "$env:USERPROFILE\AppData\Local\SteamVR\htmlcache\GPUCache"
 
 try {
-    <# abandoned
-    Get-ChildItem -Path $htmlCachePath -Recurse -Force -Filter f_* -ErrorAction Stop | Where-Object { !$_.PSIsContainer -and $_.LastWriteTime -lt $limit } | Remove-Item -Verbose
-    Get-ChildItem -Path $jsCachePath -Recurse -Force -Filter *_0 -ErrorAction Stop | Where-Object { !$_.PSIsContainer -and $_.LastWriteTime -lt $limit } | Remove-Item -Verbose
-    #>
     Get-ChildItem -Path $htmlCachePath -Recurse -Force -Filter f_* -ErrorAction Stop | Remove-Item -Verbose
     Get-ChildItem -Path $jsCachePath -Recurse -Force -Filter *_0 -ErrorAction Stop | Remove-Item -Verbose
-	
+} catch [System.Management.Automation.ItemNotFoundException] {
+    "Can't find Steam path.`nApplication may not be installed on this machine, or the folders aren't created yet."
+}
+
+try {
 	Get-ChildItem -Path $SteamVRCachePath -Recurse -Force -Filter f_* -ErrorAction Stop | Remove-Item -Verbose
 	Get-ChildItem -Path $SteamVRGPUCachePath -Recurse -Force -Filter f_* -ErrorAction Stop | Remove-Item -Verbose
 } catch [System.Management.Automation.ItemNotFoundException] {
-    "Can't find Steam path.`nApplication may not be installed on this machine, or the folders aren't created yet."
+    "Can't find SteamVR path.`nApplication may not be installed on this machine, or the folders aren't created yet."
 }
