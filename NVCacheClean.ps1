f2d3961989b65634dc4710e3a115a0b298cb1edc
azhuge233 <17763056+azhuge233@users.noreply.github.com>
2023-01-05 00:34:58 +0800

    Update NVCacheClean.ps1
    Add Nvidia DXCache

--- NVCacheClean.ps1
+++ NVCacheClean.ps1
@@ -1,7 +1,14 @@
 ﻿$nvCachePath = "C:\ProgramData\NVIDIA Corporation\NV_Cache"
+$nvDXCachePath = "$env:USERPROFILE\AppData\Local\NVIDIA\DXCache"
 
 try {
     Get-ChildItem -Path $nvCachePath -ErrorAction Stop | Remove-Item -Verbose
 } catch [System.Management.Automation.ItemNotFoundException] {
     "Can't find $($nvCachePath).`nApplication may not be installed on this machine, or the folders aren't created yet."
+}
+
+try {
+    Get-ChildItem -Path $nvDXCachePath -ErrorAction Stop | Remove-Item -Verbose
+} catch [System.Management.Automation.ItemNotFoundException] {
+    "Can't find $($nvDXCachePath).`nApplication may not be installed on this machine, or the folders aren't created yet."
 }
