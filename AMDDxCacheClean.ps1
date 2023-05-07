374b8e26d369cc766e0123893805483dca61485d
azhuge233 <17763056+azhuge233@users.noreply.github.com>
2023-05-07 09:06:08 +0800

    Update AMDDxCacheClean.ps1

--- AMDDxCacheClean.ps1
+++ AMDDxCacheClean.ps1
@@ -1,7 +1,14 @@
-﻿$AMDDxCachePath = "$env:USERPROFILE\AppData\Local\AMD\DxCache"
+$AMDDxCachePath = "$env:USERPROFILE\AppData\Local\AMD\DxCache"
+$DxcCachePath = "$env:USERPROFILE\AppData\Local\AMD\DxcCache"
 
 try {
     Get-ChildItem -Path $AMDDxCachePath -Recurse -Force -ErrorAction Stop | Remove-Item -Verbose -Recurse
 } catch [System.Management.Automation.ItemNotFoundException] {
     "Can't find $($AMDDxCachePath).`nApplication may not be installed on this machine, or the folders aren't created yet."
+}
+
+try {
+    Get-ChildItem -Path $DxcCachePath -Recurse -Force -ErrorAction Stop | Remove-Item -Verbose -Recurse
+} catch [System.Management.Automation.ItemNotFoundException] {
+    "Can't find $($D3DSCachePath).`nApplication may not be installed on this machine, or the folders aren't created yet."
 }
